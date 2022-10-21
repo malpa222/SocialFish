02b7d99952d1bd71092b307ef59668bf2d722d6c
malpa222 <d.lewandowski@student.fontys.nl>
2022-10-21 11:55:57 +0200

    GROOOOOOOOOOOOO

--- SocialFish.py
+++ SocialFish.py
@@ -540,7 +540,7 @@ def main():
         cleanFake()
         # Inicia o banco
         initDB(DATABASE)
-        app.run(host="0.0.0.0", port=5000)
+        app.run(host="0.0.0.0", port=80)
 
 if __name__ == "__main__":
     try:
